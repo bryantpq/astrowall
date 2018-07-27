e5ecc6deb4e740ed4bebadb254c2b04b84d327f1
Bryan Quah <bryanquah@gmail.com>
2018-07-27 16:41:09 +0800

    changes

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Pick your aspect ratio
+ASP_RATIO = {
+        2.3: '21:9',
+        1.7: '16:9',
+        1.6: '16:10',
+        1.3: '4:3'
+    }
+
+DEFAULT = 1.7 # 16:9 the one true res
+TOL = 0.1     # tolerance for difference in aspect ratio
